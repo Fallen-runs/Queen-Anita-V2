7674b0addd26723990fcee1219a3aa7478eaff5b
Fallen-runs <fallendiscord12@gmail.com>
2024-09-15 10:58:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3AD
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/17c8ba84a7761eed633f6.jpg";
 global.devs = "https://t.me/deecee_x";
 global.sudo = process.env.SUDO || "";
-global.owner = process.env.OWNER_NUMBER || "";
+global.owner = process.env.OWNER_NUMBER || "2347036366816";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -42,17 +42,17 @@ global.scan = "https://secret-garden-43998-4daad95d4561.herokuapp.com/";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  ""
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTUw0N2V5dk9NZVNHaWQvR2Z6NHUvNlF6OG9sNW1qSTJ4RndSd0dTNnltMD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoib3VNblp0OHYraGJWYUVkeDFQUnhSdDRiU2wxYUY2OHdQN3lseitzR0J3QT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJRSVp6WFBrVE1PZjNCeWIxbm5GNTNUY055SHFDMyt4RFh1eGNsZ1lQczJjPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJxVkZXZUF1a2prY3VCU3dxbFJoVUtHY1FwRldBSWdQL2lQeE5aMVNMTmdvPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InNLUjYxcWZxV0dXRWlzZWRGckloR0VVNEUxZWYzNGVGUndiZHlZcU1yMWc9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjlVZTVxQTF1MU1uUVI2cFlWVVMrL3pKTitiM21xd3FWSnhRcTBGMTFkVkE9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ05iN1Bza254cmlWaGtTRGlkLzlHNVJ0RklWK0FGelBaYWZWUVFsVjBuQT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicEZjQlBqL0Z4WGZMRExtQkZwcnBKWkdFalR6MGhWOHFiOG9Rc0RyZnowZz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InF0RTlkSE1pbHlEZThmSC9uQVBJT2NaQjFYWi9iY3ZZdEZpajg4ZGM2WVNtTnNUWGlXZDdBQ0tYejVkaFBsQksrY0pFVUlMRVVZanRiZ0lwaWZ3WmpRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6ODIsImFkdlNlY3JldEtleSI6IkZOdnJBdHlEUmNmOGpXdTdQWkxSSmo3RUE0TTloMlF4TjZiSEJkZGVYNFk9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6IkRHellpY3lJVHdlX2xDNzlES2lUdVEiLCJwaG9uZUlkIjoiYWE0MTZjYzctN2JjZC00NzczLWI0MDEtNWM2ZGY2YzRlZmU3IiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImJWMTRqcU1RWVB3T0puVWd2Q240a3FUcU4zbz0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJFVE1PNFVlZGVkNFc5V05BVXBuYi9JY1oyNWs9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiNVJaN0RaMTQiLCJtZSI6eyJpZCI6IjIzNDcwMzYzNjY4MTY6MTNAcy53aGF0c2FwcC5uZXQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0ttNXlJa0RFUDNXbXJjR0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6InJjeml3alBNVW1aVFljV2JFQmpmWVE0dlEvOENnZ04rQkVwN0YxcHBKUVk9IiwiYWNjb3VudFNpZ25hdHVyZSI6IjRLTVUzanBHaC8rRElJUTA4aXJZRGRDRFNvQ2RLaTJkVytFbUh0RXY2TGRKc0RlM1o4aThkbXo2RTJEK0Z2dHRHTWZjYy8xZ2Z3bmVLT0lmMCtpc0NBPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJCQzNWVTloYnJjbkJUMFE2MjZBR1UyVzFKek15SUxER3FVdWlZVXovbXpnREYva0FyOXZtT1phZ0JOR1ZuSGpOS3dSN25xZWdEYmRJYUQrdy9mSTdqQT09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjIzNDcwMzYzNjY4MTY6MTNAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCYTNNNHNJenpGSm1VMkhGbXhBWTMyRU9MMFAvQW9JRGZnUktleGRhYVNVRyJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcyNjM5MzIyNX0="
 module.exports = {
   menu: process.env.MENU || "2",
-  HANDLERS: process.env.PREFIX || ".",
+  HANDLERS: process.env.PREFIX || "null",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "`QUEEN_ANITA-V2™`",
-  author: process.env.PACK_AUTHER || "QUEEN_ANITA-V2",
-  packname: process.env.PACK_NAME || "A N I T A",
-  botname: process.env.BOT_NAME || "QUEEN_ANITA-V2",
-  ownername: process.env.OWNER_NAME || "David Cyril",
+  caption: process.env.CAPTION || "`Nigeria & Africa‘",
+  author: process.env.PACK_AUTHER || "Nigeria",
+  packname: process.env.PACK_NAME || "Africa",
+  botname: process.env.BOT_NAME || "Nigeria&Africa",
+  ownername: process.env.OWNER_NAME || "Ali redha idris",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
@@ -63,7 +63,7 @@ module.exports = {
   HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
   LANG: (process.env.THEME || "A N I T A").toUpperCase(),
 };
 global.rank = "updated";
